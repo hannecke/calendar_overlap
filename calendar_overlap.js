9a02c2e4779ad3b3527f1a44b689e0094eaad1e4
marcos.hannecke <marcos@fundment.com>
2022-06-22 15:02:41 +0100

    first

---
+++ calendar_overlap.js
@@ -0,0 +1,11 @@
+/*************
+ * CALENDAR
+ ************/
+
+// 1.Having a list of calendar events with start and end times, generate code to make sure that they wouldn’t overlap.
+
+function eventOverlap() {
+}
+
+// 2.Return list of events that overlap
+// 3.How would you improve performance?
